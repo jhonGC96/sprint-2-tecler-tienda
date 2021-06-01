2da2617f811fac8c2a5abb56eef96d8518b29d7c
Jhon <you@example.com>
2021-05-31 22:32:57 -0500

    version estable de mvc

--- app/vista/vista.login.js
+++ app/vista/vista.login.js
@@ -8,6 +8,15 @@ module.exports = async (app) => {
         res.json('ok')
     })
 
+    app.get('/paginaprincipal', async (req, res) => {
+        try {
+            res.render("paginaprincipal")
+        } catch (error) {
+            console.log(err)
+            res.estatus(400).json('No se pudo abrir la pagina')
+        }
+    })
+
     app.get('/login', async (req, res) => {
         try {
             res.render("login")
@@ -20,15 +29,11 @@ module.exports = async (app) => {
     
     app.post('/login', async (req, res) => {
         let usuario = req.body
-        //console.log(usuario);
         try {
             let resultado = await controladorLogin.chequearUsuario(usuario)
-            console.log(resultado);
             if (resultado) {
                 let tokenResult = await controladorLogin.generaToken(usuario)
-                //console.log(tokenResult);
-                //res.redirect('/')
-                res.json(tokenResult)
+                res.redirect('/paginaprincipal')
             } else {
                 throw new Error(err)
             }
